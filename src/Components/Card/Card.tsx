8214bf81a30eae69879d30c5f50d430f0f2628b1
maximeVa <maxime.vanachter@student.vinci.be>
2024-05-25 18:51:38 +0200

    add props

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -1,18 +1,23 @@
-import React from "react";
-import './Card.css';
+import "./Card.css";
 
-type Props = {};
+interface Props {
+  companyName: string;
+  ticker: string;
+  price: number;
+}
 
-const Card = (props: Props) => {
+const Card = ({ companyName, ticker, price }: Props) => {
   return (
     <div className="card">
       <img
         src="https://images.unsplash.com/photo-1612428978260-2b9c7df20150?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"
-        alt="Image"
+        alt="Boat"
       />
       <div className="details">
-        <h2>AAPL</h2>
-        <p>$110</p>
+        <h2>
+          {companyName} ({ticker})
+        </h2>
+        <p>${price}</p>
       </div>
       <p className="info">
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias officiis
