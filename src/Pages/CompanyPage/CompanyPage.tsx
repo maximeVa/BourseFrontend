e42bfd66214ff355b6f41a1e53c7aff12db0013c
maximeVa <maxime.vanachter@student.vinci.be>
2024-06-18 20:55:07 +0200

    Creating CompanyPage by multiple components + css

--- src/Pages/CompanyPage/CompanyPage.tsx
+++ src/Pages/CompanyPage/CompanyPage.tsx
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { CompanyProfile } from "../../company";
 import { getCompanyProfile } from "../../api";
+import Sidebar from "../../Components/Sidebar/Sidebar";
+import CompanyDashboard from "../../Components/CompanyDashboard/CompanyDashboard";
+import Tile from "../../Components/Tile/Tile";
 
 interface Props {}
 
@@ -19,7 +22,12 @@ const CompanyPage = (props: Props) => {
   return (
     <>
       {company ? (
-        <div className="company-profile-container">{company.companyName}</div>
+        <div className="w-full relative flex ct-docs-disable-sidebar-content overflow-x-hidden">
+          <Sidebar />
+          <CompanyDashboard>
+            <Tile title="Company name" subTitle={company.companyName}></Tile>
+          </CompanyDashboard>
+        </div>
       ) : (
         <div>Company Not Found!</div>
       )}
--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -1,8 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
-import HomePage from "../Pages/HomePage/HomePage";
 import App from "../App";
-import SearchPage from "../Pages/SearchPage/SearchPage";
+import HomePage from "../Pages/HomePage/HomePage";
 import CompanyPage from "../Pages/CompanyPage/CompanyPage";
+import SearchPage from "../Pages/SearchPage/SearchPage";
+import CompanyProfile from "../Components/CompanyProfile/CompanyProfile";
+import IncomeStatement from "../Components/IncomeStatement/IncomeStatement";
 
 export const router = createBrowserRouter([
   {
@@ -11,7 +13,14 @@ export const router = createBrowserRouter([
     children: [
       { path: "", element: <HomePage /> },
       { path: "search", element: <SearchPage /> },
-      { path: "company/:ticker", element: <CompanyPage /> },
+      {
+        path: "company/:ticker",
+        element: <CompanyPage />,
+        children: [
+          { path: "company-profile", element: <CompanyProfile /> },
+          { path: "income-statement", element: <IncomeStatement /> },
+        ],
+      },
     ],
   },
 ]);
